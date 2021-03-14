8cb421fca1edd8d2ea0b45002bf144958c95783d
baihuaxie <xie_baihua@163.com>
2021-03-14 12:38:32 +0800

    Create 300_longest_increasing_subsequence.h

---
+++ 12_dynamic_programming/300_longest_increasing_subsequence.h
@@ -0,0 +1,41 @@
+#ifndef _49_300_LONGEST_INCREASING_SUBSEQUENCE_H_
+#define _49_300_LONGEST_INCREASING_SUBSEQUENCE_H_
+#include <iostream>
+#include <vector>           //std::vector
+#include <algorithm>        //std::max
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        // dp[i] == length of longest strictly increasing subsequence ending at index=i-1
+        vector<int> dp(n + 1, 0);
+        // initialize
+        dp[0] = 1;
+
+        // running max
+        int len = 1;
+
+        // loop
+        for (int i = 1; i < n; i ++) {
+            // update
+            for (int j = i - 1; j >= 0; j--) {
+                // strictly increasing
+                if (nums[j] < nums[i])
+                    dp[i] = max(dp[i], dp[j] + 1);
+            }
+            // if nums[i] is the smallest
+            dp[i] = max(dp[i], 1);
+
+            len = max(len, dp[i]);
+        }
+
+        return len;
+    }
+};
+
+
+#endif
