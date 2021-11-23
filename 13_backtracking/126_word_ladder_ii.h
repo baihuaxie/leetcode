9c85caaa394aefe75f7b1a7865fcd8000007b2df
baihuaxie <xie_baihua@163.com>
2021-11-23 11:04:44 +0800

    11/23: create 126_word_ladder_ii.h

--- 13_backtracking/126_word_ladder_ii.h
+++ 13_backtracking/126_word_ladder_ii.h
@@ -7,7 +7,7 @@
 #include <unordered_map>    //std::unordered_map
 #include <unordered_set>    //std::unodered_set
 #include <utility>          //std::make_pair
-#include <algorithm>        //std::find
+#include <algorithm>        //std::find, std::reverse
 
 using namespace std;
 
@@ -16,18 +16,19 @@ public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
 
         // results
-        vector<vector<string>> res;
+        vector<vector<string>> paths;
+        vector<string> pth;
 
         // if endWord is not in wordList, return empty results directly;
         if (find(wordList.begin(), wordList.end(), endWord) == wordList.end())
-            return res;
+            return paths;
 
         // if beginWord is not in wordList, add it;
         if (find(wordList.begin(), wordList.end(), beginWord) == wordList.end()) {
             wordList.push_back(beginWord);
         } 
 
-        // construct a graph from wordList;
+        // initialize a graph from wordList;
         unordered_map<string, vector<string>> graph;
         for (auto word : wordList) {
             graph.insert(make_pair(word, vector<string>(0)));
@@ -46,7 +47,23 @@ public:
             }
         }
 
-        return res;
+        // use simple BFS on unweighted graph to find dist[v] == distance from v to start node;
+        unordered_map<string, int> dist;
+        dist.insert(make_pair(beginWord, 0));
+        unordered_set<string> visited;
+        bfs(beginWord, graph, dist, visited);
+
+        // backtrack from endWord, use `dist` information as conditions to avoid traversing all paths;
+        pth.push_back(endWord);
+        dfs(endWord, beginWord, graph, dist, paths, pth);
+
+        // reverse paths;
+        int num_paths = paths.size();
+        for (int i = 0; i < num_paths; ++i) {
+            reverse(paths[i].begin(), paths[i].end());
+        }
+
+        return paths;
     }
 
     bool isNeighborbyHammingDistance(string& u, string& v) {
@@ -59,9 +76,38 @@ public:
         return dist == 1 ? true : false;
     }
 
-    vector<string> bfsDijkstra(string beginWord, string endWord, unordered_map<string, vector<string>>& graph, unordered_set<string>& visited) {
-        
+    void bfs(string beginWord, unordered_map<string, vector<string>>& graph, unordered_map<string, int>& dist, unordered_set<string>& visited) {
+        queue<string> q;
+        q.push(beginWord);
+        while (!q.empty()) {
+            string u = q.front();
+            q.pop();
+            for (auto v : graph[u]) {
+                if (visited.find(v) == visited.end()) {
+                    visited.insert(v);
+                    q.push(v);
+                    dist.insert(make_pair(v, dist[u] + 1));
+                }
+            }
+        }
+    }
 
+    void dfs(string u, string beginWord, unordered_map<string, vector<string>>& graph, unordered_map<string, int>& dist,
+             vector<vector<string>>& paths, vector<string>& pth) {
+        for (auto v : graph[u]) {
+            // LIVE condition
+            if (dist[v] == dist[u] - 1) {
+                pth.push_back(v);
+                // Terminal condition
+                if (v == beginWord) {
+                    paths.push_back(pth);
+                }
+                else
+                    dfs(v, beginWord, graph, dist, paths, pth);
+                // backtrack from solution and search tree;
+                pth.pop_back();
+            }
+        }
     }
 };
 
